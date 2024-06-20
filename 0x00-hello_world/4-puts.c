4d962a72e9fa9e8e6d250a50331c29fde5c4c314
Natasha <MY_NAME@example.com>
2024-06-20 13:25:52 -0700

    5th commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** main - prints exactly "Programming is like building a multilingual puzzle, followed by a new line using puts.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
+
