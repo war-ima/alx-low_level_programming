83b829c9403fe84997ea6543147ec8c2b874538d
Natasha <MY_NAME@example.com>
2024-06-20 14:29:48 -0700

    7th commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-/*
- * main - Prints "with proper grammar,
- * but the outcome is a piece of art main".
- * using printf function
- * return: 0
+/**
+ * main - Entry point
+ * return: Always 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
