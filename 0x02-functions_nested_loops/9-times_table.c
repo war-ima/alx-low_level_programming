3bed0be943e5a40c9df050b43de56ffa0d9a40cd
Natasha <MY_NAME@example.com>
2024-06-26 15:25:31 -0700

    10th commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - prints timestable
+ */
+void times_table(void)
+{
+	int number, multiply, product;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar('0');
+
+		for (multiply = 1; multiply <= 9; multiply++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = number * multiply;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
