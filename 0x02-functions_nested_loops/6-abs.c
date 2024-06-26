96a18fce95e23a026867fe9fd6f09a0bec0ebe23
Natasha <MY_NAME@example.com>
2024-06-26 14:22:58 -0700

    7th commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value
+ * @n: integer being checked
+ * Return: Absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
