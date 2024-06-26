8f641f8948dcececb0b749bab5abf8e0a5faef62
Natasha <MY_NAME@example.com>
2024-06-26 12:56:02 -0700

    3B commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
- * main - Entry point
  * print_alphabet_x10 - prints alphabet
- * ten times
- * Return: 0
+ * 10 times.
  */
 void print_alphabet_x10(void)
 {
 	int count = 0;
 	char small;
 
-	while (count > 10)
+	while (count++ < 10)
 	{
 	for (small = 'a'; small <= 'z'; small++)
 		_putchar(small);
