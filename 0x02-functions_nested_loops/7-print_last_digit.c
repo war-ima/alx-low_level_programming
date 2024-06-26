05f5ae65e2b4196330e7bb365d52bcd0a4d076cc
Natasha <MY_NAME@example.com>
2024-06-26 14:41:45 -0700

    8th commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: value of number checked
+ * Return: last digit of number
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit = -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
