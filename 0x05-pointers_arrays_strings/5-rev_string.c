9b78023df8bd58a4225f5dc0870b2c9cd0c3d336
Natasha <MY_NAME@example.com>
2024-07-01 12:55:57 -0700

    5th commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverses string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, length, length1;
+
+	length = 0;
+	length1 = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	length1 = length - 1;
+
+	for (i = 0; i < length / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length1];
+		s[length1--] = tmp;
+	}
+}
