3d3be1385dc0be0858769a89dafc112b7acbe5e2
Natasha <MY_NAME@example.com>
2024-07-01 11:17:13 -0700

    1st commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* reset_to_98 - updates value of
+* the variable
+* @n: pointer that's updated
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+Repository for pointers and array strings project
